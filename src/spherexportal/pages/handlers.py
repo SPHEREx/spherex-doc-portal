8a018ee20cedc41b80a47b97b847f11d39a1d70f
Jonathan Sick <jonathan@jsick.codes>
2022-01-17 11:10:35 -0500

    Add jinja2-based templating of pages

--- src/spherexportal/pages/handlers.py
+++ src/spherexportal/pages/handlers.py
@@ -1,9 +1,14 @@
 """Handlers for the portal's webpages."""
 
+from pathlib import Path
+
 from fastapi import APIRouter, Depends
 from safir.dependencies.logger import logger_dependency
-from starlette.responses import HTMLResponse
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates, _TemplateResponse
 from structlog.stdlib import BoundLogger
+
+from spherexportal.repository import DocumentRepository, repository_dependency
 
 __all__ = ["router"]
 
@@ -11,8 +16,13 @@ router = APIRouter()
 """FastAPI router for webpages."""
 
 
+templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
+
+
 @router.get("/")
 async def get_homepage(
+    request: Request,
     logger: BoundLogger = Depends(logger_dependency),
-) -> HTMLResponse:
-    return HTMLResponse("<html><body><h1>Hello, world!</h1></body></html>")
+    repo: DocumentRepository = Depends(repository_dependency),
+) -> _TemplateResponse:
+    return templates.TemplateResponse("index.html", {"request": request})
