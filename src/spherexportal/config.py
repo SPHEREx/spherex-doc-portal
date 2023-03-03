77e071c963c696df4bbb18d9600650be823ab3d0
Jonathan Sick <jonathan@jsick.codes>
2023-03-03 17:23:57 -0500

    Add configurations for the LTD API

--- src/spherexportal/config.py
+++ src/spherexportal/config.py
@@ -3,8 +3,9 @@
 from __future__ import annotations
 
 from enum import Enum
+from typing import Optional
 
-from pydantic import BaseSettings, Field, FilePath
+from pydantic import BaseSettings, Field, FilePath, HttpUrl, SecretStr
 
 __all__ = ["Config", "Profile", "LogLevel"]
 
@@ -28,16 +29,40 @@ class LogLevel(str, Enum):
 
 
 class Config(BaseSettings):
-    name: str = Field("noteburst", env="SAFIR_NAME")
+    name: str = Field("spherexportal", env="SAFIR_NAME")
 
     profile: Profile = Field(Profile.production, env="SAFIR_PROFILE")
 
     log_level: LogLevel = Field(LogLevel.INFO, env="SAFIR_LOG_LEVEL")
 
-    logger_name: str = Field("noteburst", env="SAFIR_LOGGER")
+    logger_name: str = Field("spherexportal", env="SAFIR_LOGGER")
 
     dataset_path: FilePath = Field(..., env="PORTAL_DATASET_PATH")
 
+    ltd_api_url: HttpUrl = Field(
+        "https://docs-api.ipac.caltech.edu",
+        description="Root URL of the LTD API server.",
+        env="PORTAL_LTD_API_URL",
+    )
+
+    ltd_organization: str = Field(
+        "spherex",
+        description="Organization name in the LTD API.",
+        env="PORTAL_LTD_API_ORG",
+    )
+
+    ltd_api_username: str = Field(
+        "spherex-portal",
+        description="Username for LTD API",
+        env="PORTAL_LTD_API_USERNAME",
+    )
+
+    ltd_api_password: Optional[SecretStr] = Field(
+        None,
+        description="Password corresponding to ltd_api_username",
+        env="PORTAL_LTD_API_PASSWORD",
+    )
+
 
 config = Config()
-"""Configuration for noteburst."""
+"""Configuration for the spherex-doc-portal."""
