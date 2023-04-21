004d1cb5b2e0270bb75f631b2185510356c5200d
Jonathan Sick <jonathan@jsick.codes>
2023-04-21 17:46:15 -0400

    Implement refresh_projects as a cron job
    This task runs the project services's bootstrap function, which upserts
    data and therefore is safe to repeat. Note: it doesn't look for deleted
    projects.
    
    The cron job is currently configured to run every 15 minutes.

--- src/spherexportal/worker/functions/refresh.py
+++ src/spherexportal/worker/functions/refresh.py
@@ -4,8 +4,18 @@ from __future__ import annotations
 
 from typing import Any
 
+from ..servicefactory import create_project_service
+
 
 async def refresh_projects(ctx: dict[Any, Any]) -> None:
     """Refresh the cache of project data in Redis."""
     logger = ctx["logger"].bind(task="refresh_projects")
     logger.info("Starting refresh of project data")
+
+    project_service = await create_project_service(logger=logger)
+
+    # The bootstrap project refreshes the cache of project data for all
+    # projects registered with the LTD API.
+    await project_service.bootstrap_from_api()
+
+    logger.info("Finished refresh of project data")
